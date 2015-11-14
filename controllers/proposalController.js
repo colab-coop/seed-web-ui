816c4b5d02e964ac53bacc986d0f29f8cb5d2463
elliottjf <elliottj@pobox.com>
2015-11-14 08:16:44 -0500

    sorting hack

--- controllers/proposalController.js
+++ controllers/proposalController.js
@@ -105,7 +105,7 @@ function listProposalsView(req, res, view, kind) {
       }
       return count == 0
         ? Promise.resolve([])
-        : Proposal.find(filter, null, {skip: skip, limit: count}).populate('parentRef profileRef');  // TODO: confirm if this cascades and if we need to worry about circular refs
+        : Proposal.find(filter, null, {skip: skip, limit: count}).sort({'title': -1}).populate('parentRef profileRef');  // TODO: confirm if this cascades and if we need to worry about circular refs
     })
     .then((items) => {
       model.items = items;

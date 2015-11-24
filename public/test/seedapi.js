bcb7e9328b6123ac1c439931c812e64e7d0bb680
elliottjf <elliottj@pobox.com>
2015-11-23 22:46:37 -0800

    seedapi.js factoring

--- public/test/seedapi.js
+++ public/test/seedapi.js
@@ -1,86 +1,123 @@
 
-var SeedApi = {
+var SeedApiSinglton = (function() {
 
-//  var endpoint = 'http://seedbombing.dev.colab.coop';
-//  var campaignId = 'NkvHV25Ql';
+  var instance;
 
-  endpoint: 'http://localhost:8000',
-  campaignId: 'VJ7iA5cQg',
+  function init() {
+
+    //note: assumes a block like this is included subsequent to this library being loaded
+    //SeedApi.setEndpoint('http://localhost:8000');
+    //SeedApi.setApiKey('w4l');
+    //SeedApi.setCampaignId('VJ7iA5cQg');
+
 
 //  var endpoint = 'http://seed.stage.colab.coop';
 //  var campaignId = 'N1bm2FjXx';
-  apiKey: 'w4l',
+
+  //var endpoint = 'http://seedbombing.dev.colab.coop';
+  //var campaignId = 'NkvHV25Ql';
+
+    var endpoint;// = 'http://localhost:8000';
+    var campaignId;// = 'VJ7iA5cQg';
+    var apiKey; // = 'w4l';
 
 
-  submitPledge: function(data, successHandler) {
-    var uri = '/api/v1/campaign/' + data.campaignId + '/pledge.submit';
-    invoke(uri, data, successHandler);
-  },
 
-  submitPaymentInfo: function(data, successHandler) {
-    var uri = '/api/v1/contribution/' + data.contributionId + '/paymentInfo.submit';
-    invoke(uri, data, successHandler);
-  },
-
-  fetchContributionStatus: function(data, successHandler) {
-    var uri = '/api/v1/contribution/' + data.contributionId + '/status';
-    invoke(uri, data, successHandler);
-  },
-
-  endRecurringContribution: function(data, successHandler) {
-    var uri = '/api/v1/contribution/' + data.contributionId + '/endRecurring';
-    invoke(uri, data, successHandler);
-  },
-
-
-  copyFormValues: function(data, form, fields) {
-    for (i = 0; i < fields.length; i++) {
-      data[fields[i]] = formValue(form, fields[i]);
+    function submitPledge(data, successHandler) {
+      var uri = '/api/v1/campaign/' + data.campaignId + '/pledge.submit';
+      invoke(uri, data, successHandler);
     }
-  },
 
-
-  getParameterByName: function(name) {
-    var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
-    return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
-  },
-
-  invoke: invoke,
-  formValue: formValue,
-
-};
-
-function endpoint() {
-  return SeedApi.endpoint;
-}
-
-function campaignId() {
-  return SeedApi.campaignId;
-}
-
-function apiKey() {
-//  "use strict";
-  return SeedApi.apiKey;
-}
-
-function invoke(uri, data, successHandler) {
-  var url = endpoint() + uri;
-  data.apiKey = apiKey();
-  $.ajax({
-    url: url
-    , dataType: 'jsonp'
-    , data: data
-    , success: successHandler
-    , error: function (xhr, status, error) {
-      alert(status)
+    function submitPaymentInfo(data, successHandler) {
+      var uri = '/api/v1/contribution/' + data.contributionId + '/paymentInfo.submit';
+      invoke(uri, data, successHandler);
     }
-  });
-}
 
-function formValue(form, field) {
-  return form.elements[field].value;
-}
+    function fetchContributionStatus(data, successHandler) {
+      var uri = '/api/v1/contribution/' + data.contributionId + '/status';
+      invoke(uri, data, successHandler);
+    }
+
+    function endRecurringContribution(data, successHandler) {
+      var uri = '/api/v1/contribution/' + data.contributionId + '/endRecurring';
+      invoke(uri, data, successHandler);
+    }
+
+    function invoke(uri, data, successHandler) {
+      var url = endpoint + uri;
+      data.apiKey = apiKey;
+      $.ajax({
+        url: url
+        , dataType: 'jsonp'
+        , data: data
+        , success: successHandler
+        , error: function (xhr, status, error) {
+          alert(status)
+        }
+      });
+    }
+
+    function formValue(form, field) {
+      return form.elements[field].value;
+    }
+
+    function copyFormValues(data, form, fields) {
+      for (i = 0; i < fields.length; i++) {
+        data[fields[i]] = formValue(form, fields[i]);
+      }
+    }
+
+    function getParameterByName(name) {
+      var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
+      return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+    }
 
 
+    function setEndpoint(s) {
+      endpoint = s;
+    }
 
-//module.exports = SeedApi;
+    function setApiKey(s) {
+      apiKey = s;
+    }
+
+    function setCampaignId(s) {
+      campaignId = s;
+    }
+
+    function getCampaignId() {
+      return campaignId;
+    }
+
+    return {
+      submitPledge: submitPledge,
+      submitPaymentInfo: submitPaymentInfo,
+      fetchContributionStatus: fetchContributionStatus,
+      endRecurringContribution: endRecurringContribution,
+      invoke: invoke,
+      copyFormValues: copyFormValues,
+      formValue: formValue,
+      getParameterByName: getParameterByName,
+      setEndpoint: setEndpoint,
+      setApiKey: setApiKey,
+      setCampaignId: setCampaignId,
+      getCampaignId: getCampaignId
+    };
+  }
+
+  return {
+    getInstance: function() {
+      "use strict";
+      if (!instance) {
+        instance = init();
+      }
+      return instance;
+    }
+  };
+
+})();
+
+var SeedApi = SeedApiSinglton.getInstance();
+window.SeedApi = SeedApi;
+
+
--- public/test/seedapiconfig.js
+++ public/test/seedapiconfig.js
@@ -7,8 +7,8 @@
 //SeedApi.apiKey = 'w4l';
 //SeedApi.campaignId = 'NkvHV25Ql';
 
-SeedApi.endpoint = 'http://localhost:8000';
-SeedApi.apiKey = 'w4l';
-SeedApi.campaignId = 'VJ7iA5cQg';
+SeedApi.setEndpoint('http://localhost:8000');
+SeedApi.setApiKey('w4l');
+SeedApi.setCampaignId('VJ7iA5cQg');
 
 

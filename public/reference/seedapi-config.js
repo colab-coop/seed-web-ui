f9a5aba5ef5e247b797c87f6716350c4a8cb9b73
elliottjf <elliottj@pobox.com>
2015-11-24 00:37:29 -0800

    point ref to stage

--- public/reference/seedapi-config.js
+++ public/reference/seedapi-config.js
@@ -1,10 +1,10 @@
 
-//SeedApi.setEndpoint('http://seed.stage.colab.coop');
-//SeedApi.setApiKey('w4l');
-//SeedApi.setCampaignId('N1bm2FjXx');
-
-SeedApi.setEndpoint('http://localhost:8000');
+SeedApi.setEndpoint('http://seed.stage.colab.coop');
 SeedApi.setApiKey('w4l');
-SeedApi.setCampaignId('VJ7iA5cQg');
+SeedApi.setCampaignId('N1bm2FjXx');
+
+//SeedApi.setEndpoint('http://localhost:8000');
+//SeedApi.setApiKey('w4l');
+//SeedApi.setCampaignId('VJ7iA5cQg');
 
 
